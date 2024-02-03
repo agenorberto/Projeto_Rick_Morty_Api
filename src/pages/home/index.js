ed845128410f4c3146dd44414bf6ab9c2fc9b87c
Agenor Berto <agberto@gmail.com>
2024-02-03 13:46:16 -0300

    v 0.2

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <>
             <div className="card flex justify-content-center">
-                    <Image src="/image/rick-morty.jpg" 
+                    <Image src="/image/rick_morty.jpg" 
                         alt="rick and morty" width="250" />
                 </div>
                 <div className="card flex justify-content-center">
@@ -12,12 +12,10 @@ const Home = () => {
                         Trabalho realizado com react e consumo de API Rick and Morty.
                     </p>
                 </div>
-                <div className="card flex justify-content-center">
+                  <div className="card flex justify-content-center">
                     <p>
-                        Foi criando páginas e componentes para reaproveitamento de código.
-                        Nesse projeto foi utilizado um biblioteca PrimeReact,
-                        ela disponibiliza componentes já prontos para serem utilizadas na criação de páginas.
-                    </p>   
+                        Projeto Rick and Morty - Projeto Disciplina Arquitetura Front End.
+                    </p>
                 </div>
         </>
     )
